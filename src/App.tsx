c6ba014a66d22d04497c1e2465159950a27f57ae
cj <cjvuoksi@gmail.com>
2025-02-12 19:22:38 -0700

    Added routing structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,46 @@
 import './App.css'
+import {BrowserRouter,
+    Route,
+    Routes,
+} from "react-router-dom"
+
+import participant from "./pages/participant.tsx";
+import presenter from "./pages/presenter.tsx";
+import ErrorPage from "./pages/ErrorPage.tsx";
+
+import {useDevice, DeviceType} from "./components/device/useDevice.ts";
+
+// Consider checking connection to Wi-Fi using navigator.connection
 
 function App() {
 
-  return (
-    <>
-      <h1>Official-Unoffical Mic-Runner Starter Code 💃🕺</h1>
-    </>
-  )
+    // presenterRoutes if we want to do logic with getting the device type (mobile vs desktop)
+    const presenterRoutes = () => {
+        return (
+            <Routes>
+                <Route index element={presenter()} />
+                <Route path='*' element={ErrorPage()} />
+            </Routes>
+        )
+    }
+
+    // participant routes for mobile devices
+    const participantRoutes = () => {
+        return (
+            <Routes>
+                <Route index element={participant()} />
+                <Route path='*' element={ErrorPage()} />
+            </Routes>
+        )
+    }
+
+    return (
+      <BrowserRouter>
+          {
+            (useDevice() == DeviceType.web) ? (presenterRoutes()) : (participantRoutes())
+          }
+      </BrowserRouter>
+    )
 }
 
 export default App
