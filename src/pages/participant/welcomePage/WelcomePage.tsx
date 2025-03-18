6bfc95c0d0974f9cfe9278f3fc9bb11da4281ec2
cj <cjvuoksi@gmail.com>
2025-03-17 20:02:58 -0600

    modified welcomePage to accept redirects

--- src/pages/participant/welcomePage/WelcomePage.tsx
+++ src/pages/participant/welcomePage/WelcomePage.tsx
@@ -1,7 +1,38 @@
 import HostRoom from "../hostRoom/HostRoom";
 import JoinRoom from "../joinRoom/JoinRoom";
+import {useNavigate} from "react-router-dom";
+import {useEffect} from "react";
+
+
 
 function WelcomePage() {
+  const navigate = useNavigate()
+
+  useEffect(() => {
+      const currURL = new URL(location.href);
+      const redirectStr = currURL.searchParams.get("redirect");
+      if (redirectStr) {
+        const redirect = JSON.parse(decodeURIComponent(redirectStr));
+        let path = ""
+        if (redirect.path) {
+            path = redirect.path.replace(import.meta.env.VITE_APP_BASENAME, "");
+        }
+
+        let navigationStr: string = path + "?"
+        console.log(navigationStr);
+        console.log(redirect.searches);
+        if (redirect.searches) {
+            for (const [key, value] of Object.entries(redirect.searches)) {
+                console.log(key, value);
+                navigationStr += key + "=" + value;
+            }
+        }
+        console.log(navigationStr);
+        navigate(navigationStr);
+
+      }
+  }, [])
+
   return (
     <div id="participant-layout">
       <div id="participant-header">
