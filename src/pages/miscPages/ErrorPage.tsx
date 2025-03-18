cac9048bdbaa7bab8ea3710afbcfe80d116ec8c7
cj <cjvuoksi@gmail.com>
2025-03-18 11:52:24 -0600

    updated error page with link to home

--- src/pages/miscPages/ErrorPage.tsx
+++ src/pages/miscPages/ErrorPage.tsx
@@ -1,9 +1,13 @@
+import {NavLink} from "react-router-dom";
 
 
 const ErrorPage = () => {
     return (
         <div>
             <h1>404 Error: Not found</h1>
+            <div style={ {padding: '1em', textAlign: 'center'}}>
+                <NavLink to={"/"}>Go to home</NavLink>
+            </div>
         </div>
     )
 }
