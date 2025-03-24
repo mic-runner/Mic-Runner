777aae1681812cb32a6f742ec1359dcf3aa9e5a5
connerwattles <70725683+connerwattles@users.noreply.github.com>
2025-03-24 11:14:26 -0600

    back to old vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,8 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
+// https://vite.dev/config/
 export default defineConfig({
-  base: "/",
+  base: process.env.NODE_ENV === "production" ? "/Mic-Runner/" : "/",
   plugins: [react()],
 });
